2c4ea5dc8b2841b81a191d1c56a8b38830961ac4
ActonMartin <wszcla@outlook.com>
2020-05-16 16:50:53 +0800

    SVG2EMF

--- convSVG2EMF.py
+++ convSVG2EMF.py
@@ -14,22 +14,25 @@ class convertSVGtoEMF(object):
     
     def startcovert(self):
         files = os.listdir(self.folder)
+        count = 0
         for file in files:
-            count = 0
-            svg_list = []
             if file.endswith('.svg'):
-                svg_list.append(file)
                 svg_path = self.folder + file
-                emf_path = self.folder + 'EMF/' + file.split('.')[0] + '.emf' 
-                subprocess.run([self.inkscapePath, svg_path, '-M', emf_path])
+                emf_folder = self.folder + 'EMF/'
+                emf_path = emf_folder + file.split('.')[0] + '.emf'
+                print(emf_path,'开始进行转换')
+                if not os.path.exists(emf_folder):
+                    os.makedirs(emf_folder)
                 count += 1
-            if count == len(svg_list):
-                print("所有的svg图像已经转换成功")
-                
+                cmd = "{0} -p {1} -o {2}".format(self.inkscapePath, svg_path, emf_path)
+                process = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        if count == len(files):
+            print("所有的svg图像已经转换成功")
+
 
 if __name__ == "__main__":
-    inkscapePath = ''
-    folder = ''
+    inkscapePath = 'C:/Program Files/Inkscape/bin/inkscape.com'
+    folder = 'E:/lll/'
     convertSVGtoEMF(inkscapePath, folder).startcovert()
 
     
